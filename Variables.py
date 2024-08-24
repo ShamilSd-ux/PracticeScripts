ad311583d026f31d02c445abb1e0e3b7bbb74590
ShamilSd-ux <81691084+ShamilSd-ux@users.noreply.github.com>
2024-08-24 14:03:37 +0530

    Bismillah

---
+++ Variables.py
@@ -0,0 +1,7 @@
+# Example of variables and data types
+x = 5              # Integer
+y = 3.14           # Float
+name = "AmeeruddinSyed"     # String
+is_student = True  # Boolean
+
+print(x, y, name, is_student)
